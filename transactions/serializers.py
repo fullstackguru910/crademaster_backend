af671375495d0a2ee4f19f7c3013c11a393067db
fullstackguru910 <enterprise.king9@gmail.com>
2025-02-07 11:26:46 +0900

    wallet address validation added

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,3 +1,5 @@
+from tronpy import Tron
+from django.db.models import Sum
 from rest_framework import serializers
 
 from .models import Transaction
@@ -20,6 +22,14 @@ class WithdrawSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'amount', 'address', 'status', 'requested_at', 'completed_at']
         read_only_fields = ['id', 'user', 'status', 'requested_at', 'completed_at']
 
+    def validate_address(self, value):
+        """
+        Validate the withdrawal address format.
+        """
+        if not Tron().is_address(value):
+            raise serializers.ValidationError("Invalid Tron address.")
+        return value
+
     def validate(self, data):
         user = self.context['request'].user
         if Transaction.objects.filter(user=user, status='PENDING').exists():
@@ -30,4 +40,10 @@ class WithdrawSerializer(serializers.ModelSerializer):
 
         if data['address'] == user.cm_wallet:
             raise serializers.ValidationError("Withdrawal address shouldn't be the crademaster wallet.")
+        
+        invests = user.events.aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+
+        if data['amount'] > user.get_balance - float(invests):
+            raise serializers.ValidationError("You can not withdraw event amount.")
+
         return data
