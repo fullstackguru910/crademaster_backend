30b74df93aefa3cbf68c266aedc9772e180958c6
fullstackguru910 <enterprise.king9@gmail.com>
2025-03-06 23:29:17 +0900

    django media url pattern added

--- crademaster/urls.py
+++ crademaster/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView, TemplateView
@@ -40,3 +42,5 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
