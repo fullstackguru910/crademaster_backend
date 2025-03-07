8a2e55e00039bfe0f68cdfff72dcf503d3e71562
fullstackguru910 <enterprise.king9@gmail.com>
2025-03-08 00:37:09 +0900

    remove user from event

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import (
-    EventListView, EventCreateView, EventUpdateView
+    EventListView,
+    EventCreateView,
+    EventUpdateView,
+    UserEventListView,
+    RemoveUserFromEventView,
 )
 
 urlpatterns = [
     path('', EventListView.as_view(), name='event_list'),
     path('create/', EventCreateView.as_view(), name='event_create'),
     path('<int:pk>/edit/', EventUpdateView.as_view(), name='event_update'),
+    path('user/<int:pk>/', UserEventListView.as_view(), name='user_event_list'),
+    path('<int:event_id>/user/<int:user_id>/remove/', RemoveUserFromEventView.as_view(), name='remove_user_from_event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,13 +1,37 @@
+from django.views import View
 from django.urls import reverse_lazy
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, UpdateView, CreateView
+from django.contrib.auth import get_user_model
 
 from authentication.mixins import StaffRequiredMixin
 from .models import Event
+
+User = get_user_model()
+
 
 class EventListView(StaffRequiredMixin, ListView):
     model = Event
     template_name = 'events/list.html'
     context_object_name = 'events'
+
+
+class UserEventListView(StaffRequiredMixin, ListView):
+    model = Event
+    template_name = 'events/user/list.html'
+    context_object_name = 'events'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(users__pk=self.kwargs.get('pk'))
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_pk = self.kwargs.get('pk')
+        user = get_object_or_404(User, pk=user_pk)
+        context['user'] = user
+        return context
 
 
 class EventCreateView(StaffRequiredMixin, CreateView):
@@ -26,3 +50,17 @@ class EventUpdateView(StaffRequiredMixin, UpdateView):
 
     def get_object(self):
         return Event.objects.get(id=self.kwargs['pk'])
+
+
+class RemoveUserFromEventView(View):
+    def post(self, request, event_id, user_id):
+        event = get_object_or_404(Event, pk=event_id)
+        user = get_object_or_404(User, id=user_id)
+
+        if user not in event.users.all():
+            return HttpResponseForbidden("User is not part of the event")
+
+        event.users.remove(user)
+        
+        # Redirect or return a response
+        return redirect('user_list')
