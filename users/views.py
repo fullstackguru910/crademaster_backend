07e773d5c3f001fbe383eb7371122d9a265da8c7
fullstackguru910 <enterprise.king9@gmail.com>
2025-03-08 01:44:05 +0900

    trongrid api added to user list template

--- users/views.py
+++ users/views.py
@@ -15,7 +15,6 @@ class UserListView(StaffRequiredMixin, ListView):
     model = CustomUser
     template_name = 'users/list.html'
     context_object_name = 'users'
-    # paginate_by = 5
 
     def get_queryset(self):
         queryset = super().get_queryset()
